edf28eaa1c9f7a07f279fe49687af7f849931ae0
akshun <akshun.mishra.che21@itbhu.ac.in>
2023-02-26 15:47:18 +0530

    Home page updated

---
+++ sochem2.0/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from "../components/Home/hero"
+import Main from "../components/Home/main"
+const home = () => {
+  return (
+    <div>
+        <Hero/>
+        <Main/>
+    </div>
+  )
+}
+
+export default home
