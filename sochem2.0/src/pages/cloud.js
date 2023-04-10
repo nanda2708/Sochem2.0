984268f10b07c543bc437bdcd82a1d6996018b78
iamkapil <kapilrhngdle2211@gmail.com>
2023-04-11 00:19:25 +0530

    cloud page updated

--- sochem2.0/src/pages/cloud.js
+++ sochem2.0/src/pages/cloud.js
@@ -1,174 +1,174 @@
-import Titlebar from "@/components/title";
+
 import Navbar from "@/components/navbar";
-import { SectionTitle } from "@/components/section";
 import Link from "next/link";
+import Footer from '@/components/footer.js'
 
  const Cloud=()=>{
   return (
     <div>
       <Navbar/>
-      <div className="bg-slate-900">
+      <div className="bg-[#040D21]">
         <div>
-        <p className='font-medium flex text-[#F7FAFF] justify-center text-2xl sm:text-3xl lg:text-5xl'><p className='mt-12 mb-12'>Resources Vault</p></p>
+        <div className='font-medium flex text-[#F7FAFF] justify-center text-2xl sm:text-3xl lg:text-5xl'><div className='mt-12 mb-12'>Resources Vault</div></div>
           
         </div>
 
-        <div className=" mr-12 ml-12 gap-12 pr-12 pl-12 flex flex-row  ">
-          <div className="mb-5 ml-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
-            <a href="https://drive.google.com/drive/folders/1TOxyPEmb8l9VvNAa1IycT7rBHIp2x_Wd?usp=sharing">
+        <div className=" ml-12 pl-10 flex flex-row  ">
+          <div className=" h-96 mt-8 ml-10 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
+            <Link href="https://drive.google.com/drive/folders/1TOxyPEmb8l9VvNAa1IycT7rBHIp2x_Wd?usp=sharing">
               <div className="w-full h-full">
                 <img className="object-scale-down h-48 w-96" src="/img/Books.png" />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
-                    Books/Novels
+                    Books/Novels/Research Papers
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-10  mr-12 ml-12 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
-            <a href="https://drive.google.com/drive/folders/15pTBbGVUdA0dTGFQI7QpfQWaXDQsr6cy?usp=sharing">
+          <div className=" h-96 mb-10 ml-12 mr-12  shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
+            <Link  href="https://drive.google.com/drive/folders/15pTBbGVUdA0dTGFQI7QpfQWaXDQsr6cy?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Core_courses.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className=" font-bold text-xl mb-2 card-text text-center text-slate-300 ">
                     Core Courses
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-5 mr-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
-            <a href="https://drive.google.com/drive/folders/1R34YagE19rLI31yMRQo4G-UnvHRRBmgn?usp=sharing">
+          <div className=" h-96 mt-8  shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
+            <Link href="https://drive.google.com/drive/folders/1R34YagE19rLI31yMRQo4G-UnvHRRBmgn?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Gate_preparation.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
                     Gate Preparation
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
         </div>
 
-        <div className=" mr-12 ml-12 gap-12 pr-12 pl-12 flex flex-row  ">
-          <div className="mb-5 ml-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
-            <a href="https://drive.google.com/drive/folders/1mZSAyhN7BNX51g9V4if-6P1NWyFqtCmm?usp=sharing">
+        <div className="  ml-12 pl-10 flex flex-row  ">
+          <div className=" h-96 mt-8 ml-10 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
+            <Link href="https://drive.google.com/drive/folders/1mZSAyhN7BNX51g9V4if-6P1NWyFqtCmm?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Gre_preparation.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-ml  card-text text-center text-slate-300">
                     GRE Preparation
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-10  mr-12 ml-12 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
-            <a href="https://drive.google.com/drive/folders/18u9EqfPqZ7t9z9RL49WL1ebeW51t_eF-?usp=sharing">
+          <div className="h-96 mb-10 ml-12 mr-12    shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
+            <Link href="https://drive.google.com/drive/folders/18u9EqfPqZ7t9z9RL49WL1ebeW51t_eF-?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Data_analyst.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl  card-text text-center text-slate-300 ">
-                    HULM
+                    HU/LM/OE Courses
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-5 mr-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
-            <a href="https://drive.google.com/drive/folders/1ZGKENtgtA8UTCHcWIlSveJxg9_uX44vt?usp=sharing">
+          <div className="h-96 mt-8 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 ">
+            <Link href="https://drive.google.com/drive/folders/1ZGKENtgtA8UTCHcWIlSveJxg9_uX44vt?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Online_courses.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
                     Online Courses
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
         </div>
 
-        <div className="mb-10  mr-12 ml-12 gap-12 pr-12 pl-12 flex flex-row  ">
-          <div className="mb-5 ml-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
-            <a href="https://drive.google.com/drive/folders/1-FPXGNsfx7jcpLksfqqyo_7KA0KeOBi9?usp=sharing">
+        <div className=" ml-12 pl-10 flex flex-row  ">
+          <div className=" h-96 mt-8 ml-10 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
+            <Link href="https://drive.google.com/drive/folders/1-FPXGNsfx7jcpLksfqqyo_7KA0KeOBi9?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Books.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
                     Open Electives
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-10  mr-12 ml-12 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
-            <a href="https://drive.google.com/drive/folders/1VYASsgV_Ods_EyHiKd_Sm7qjE1gyNpnU?usp=sharing">
+          <div className="h-96 mb-10 ml-12 mr-12 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
+            <Link href="https://drive.google.com/drive/folders/1VYASsgV_Ods_EyHiKd_Sm7qjE1gyNpnU?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/programming.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
                     Programming
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
 
-          <div className="mb-5 mr-12 mt-8 shadow-2xl bg-[#312e81] rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
-            <a href="https://drive.google.com/drive/folders/1VYASsgV_Ods_EyHiKd_Sm7qjE1gyNpnU?usp=sharing">
+          <div className="h-96 mt-8 shadow-2xl bg-slate-900 rounded border-2 border-sky-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300   ">
+            <Link href="https://drive.google.com/drive/folders/1VYASsgV_Ods_EyHiKd_Sm7qjE1gyNpnU?usp=sharing">
               <div className="w-full h-full">
                 <img
                   className="object-scale-down h-48 w-96 "
                   src="/img/Data_analyst.png"
                 />
-                <div className="py-5">
+                <div className="py-10">
                   <div className="font-bold text-xl mb-10 card-text text-center text-slate-300">
                     Data Analyst
                   </div>
                 </div>
               </div>
-            </a>
+            </Link>
           </div>
+
+          
         </div>
       </div>
+
+
+
+      <div>  <Footer/></div>
     </div>
   );
 }
 
 export default Cloud
 
-/*
-
-
-      
-
-*/
