8ae943a27e5298fc520eb2c9228ca04ea996c4f8
shruti <shrutiyadav862@gmail.com>
2024-10-28 00:19:48 +0530

    signup & signin functionality added

---
+++ sochem2.0/services/db_services.js
@@ -0,0 +1,32 @@
+import { db } from "@/firebase/firebaseConfig";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+
+const dbRef = collection(db, "members");
+class MembersDataService {
+  addMembers = (member) => {
+    return addDoc(dbRef, member);
+  };
+
+  deleteMember = (id) => {
+    const member = doc(db, "members", id);
+    return deleteDoc(member);
+  };
+}
+
+async function verifyEmail(email) {
+  const q = query(dbRef, where("email", "==", email));
+  const querySnapshot = await getDocs(q);
+
+  if (!querySnapshot.empty) return true;
+  else return false;
+}
+const membersDataService = new MembersDataService();
+export { membersDataService, verifyEmail };
